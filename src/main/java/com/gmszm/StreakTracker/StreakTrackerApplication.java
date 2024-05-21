c1b257ffe90b93138ee8fc846c03630c1b6d575f
Gabriella Szebeni-Molnár <gabriella.szebeni@gmail.com>
2024-05-21 09:48:11 +0200

    base project created

---
+++ src/main/java/com/gmszm/StreakTracker/StreakTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.gmszm.StreakTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreakTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreakTrackerApplication.class, args);
+	}
+
+}
